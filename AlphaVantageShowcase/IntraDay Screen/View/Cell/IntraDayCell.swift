81cc67aba250b9a0a098e19aaf1a6c7ff9964813
Ganang Arief Pratama <ganang.arief.p@gmail.com>
2021-01-09 17:39:41 +0700

    add sorting feature intraday

--- AlphaVantageShowcase/IntraDay Screen/View/Cell/IntraDayCell.swift
+++ AlphaVantageShowcase/IntraDay Screen/View/Cell/IntraDayCell.swift
@@ -9,11 +9,31 @@ import UIKit
 
 class IntraDayCell: UITableViewCell {
     
+    let dateLabelHeader: UILabel = {
+        let label = UILabel()
+        label.translatesAutoresizingMaskIntoConstraints = false
+        label.textColor = .black
+        label.font = .boldSystemFont(ofSize: 12)
+        label.text = "Date/Time"
+        
+        return label
+    }()
+    
     let dateLabel: UILabel = {
         let label = UILabel()
         label.translatesAutoresizingMaskIntoConstraints = false
         label.textColor = .black
         label.font = .boldSystemFont(ofSize: 12)
+        
+        return label
+    }()
+    
+    let openLabelHeader: UILabel = {
+        let label = UILabel()
+        label.translatesAutoresizingMaskIntoConstraints = false
+        label.textColor = .black
+        label.font = .boldSystemFont(ofSize: 12)
+        label.text = "Open"
         
         return label
     }()
@@ -27,11 +47,31 @@ class IntraDayCell: UITableViewCell {
         return label
     }()
     
+    let lowLabelHeader: UILabel = {
+        let label = UILabel()
+        label.translatesAutoresizingMaskIntoConstraints = false
+        label.textColor = .black
+        label.font = .boldSystemFont(ofSize: 12)
+        label.text = "Low"
+        
+        return label
+    }()
+    
     let lowLabel: UILabel = {
         let label = UILabel()
         label.translatesAutoresizingMaskIntoConstraints = false
         label.textColor = .black
         label.font = .systemFont(ofSize: 12)
+        
+        return label
+    }()
+    
+    let highLabelHeader: UILabel = {
+        let label = UILabel()
+        label.translatesAutoresizingMaskIntoConstraints = false
+        label.textColor = .black
+        label.font = .boldSystemFont(ofSize: 12)
+        label.text = "High"
         
         return label
     }()
@@ -61,19 +101,36 @@ class IntraDayCell: UITableViewCell {
         addSubview(openLabel)
         addSubview(highLabel)
         addSubview(lowLabel)
+        
+        addSubview(dateLabelHeader)
+        addSubview(openLabelHeader)
+        addSubview(highLabelHeader)
+        addSubview(lowLabelHeader)
     }
     
     func setupConstraint() {
-        dateLabel.topAnchor.constraint(equalTo: topAnchor, constant: 8).isActive = true
-        dateLabel.leadingAnchor.constraint(equalTo: leadingAnchor,constant: 8).isActive = true
+        dateLabelHeader.topAnchor.constraint(equalTo: topAnchor, constant: 8).isActive = true
+        dateLabelHeader.leadingAnchor.constraint(equalTo: leadingAnchor,constant: 16).isActive = true
         
-        openLabel.centerYAnchor.constraint(equalTo: dateLabel.bottomAnchor, constant: 8).isActive = true
-        openLabel.leadingAnchor.constraint(equalTo: leadingAnchor,constant: 8).isActive = true
+        dateLabel.topAnchor.constraint(equalTo: dateLabelHeader.bottomAnchor, constant: 8).isActive = true
+        dateLabel.leadingAnchor.constraint(equalTo: dateLabelHeader.leadingAnchor).isActive = true
         
-        highLabel.centerYAnchor.constraint(equalTo: dateLabel.bottomAnchor, constant: 8).isActive = true
+        openLabelHeader.topAnchor.constraint(equalTo: topAnchor, constant: 8).isActive = true
+        openLabelHeader.leadingAnchor.constraint(equalTo: dateLabelHeader.trailingAnchor, constant: 72).isActive = true
+        
+        openLabel.topAnchor.constraint(equalTo: openLabelHeader.bottomAnchor, constant: 8).isActive = true
+        openLabel.leadingAnchor.constraint(equalTo: dateLabel.trailingAnchor,constant: 8).isActive = true
+        
+        highLabelHeader.topAnchor.constraint(equalTo: topAnchor, constant: 8).isActive = true
+        highLabelHeader.leadingAnchor.constraint(equalTo: openLabelHeader.trailingAnchor, constant: 36).isActive = true
+        
+        highLabel.topAnchor.constraint(equalTo: highLabelHeader.bottomAnchor, constant: 8).isActive = true
         highLabel.leadingAnchor.constraint(equalTo: openLabel.trailingAnchor,constant: 8).isActive = true
         
-        lowLabel.centerYAnchor.constraint(equalTo: dateLabel.bottomAnchor, constant: 8).isActive = true
+        lowLabelHeader.topAnchor.constraint(equalTo: topAnchor, constant: 8).isActive = true
+        lowLabelHeader.leadingAnchor.constraint(equalTo: highLabelHeader.trailingAnchor, constant: 36).isActive = true
+        
+        lowLabel.topAnchor.constraint(equalTo: lowLabelHeader.bottomAnchor, constant: 8).isActive = true
         lowLabel.leadingAnchor.constraint(equalTo: highLabel.trailingAnchor,constant: 8).isActive = true
     }
     
